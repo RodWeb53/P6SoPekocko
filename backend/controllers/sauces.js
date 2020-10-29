1f082bca7ad89a359fdae3e0ff77e82e157267bd
RodWeb53 <vizoro@wanadoo.fr>
2020-10-29 09:18:31 +0100

    Correction sur controle user Id

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -30,16 +30,31 @@ exports.createSauces = (req, res, next) => {
 
 
 //------------- Modification d'une sauce --------------\\
+//suite passage soutenance modification pour jouter un controle sur l'ID du user
+//Fonctionne si on ne modifie pas l'image sinon bug
+//Met à jour la sauce et son image via son ID
 exports.modifySauces = (req, res, next) => {
-  const thingObject = req.file ?
-    {
-      ...JSON.parse(req.body.sauce),
-      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-    } : { ...req.body };
-  Sauce.updateOne({ _id: req.params.id }, { ...thingObject, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-    .catch(error => res.status(400).json({ error }));
+  Sauce.findOne({ _id: req.params.id })
+    .then((sauce) => {
+      console.log('Modification : sauce.userId : ' + sauce.userId); //ok dans les 2 types de modification
+      console.log('Modification : req.body.userId : ' + req.body.userId); //Ok si on ne change pas l'image
+      if (sauce.userId === req.body.userId) {
+        const thingObject = req.file ?
+        {
+          ...JSON.parse(req.body.sauce),
+          imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+        } : { ...req.body };
+        Sauce.updateOne({ _id: req.params.id }, { ...thingObject, _id: req.params.id })
+          .then(() => res.status(200).json({ message: 'Objet modifié !' }))
+          .catch(error => res.status(400).json({ error }));
+      } else {
+          throw 'Impossible de modifier la sauce.';
+      }
+    })
+    .catch(error => res.status(500).json({ error }));
 };
+
+
 //------------- suppression d'une sauce ------------------\\
 exports.deleteSauces = (req, res, next) => {
   //recupération de l'id avec la méthodes findOne
@@ -51,7 +66,7 @@ exports.deleteSauces = (req, res, next) => {
         //suppression de la BD avec la méthode deleteOne
         Sauce.deleteOne({ _id: req.params.id })
           .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-          .catch(error => res.status(400).json({ error }));
+          .catch(error => res.status(400).json({ error })); 
       });
     })
     .catch(error => res.status(500).json({ error }));
@@ -68,38 +83,34 @@ exports.getAllSauces = (req, res, next) => {
 //------- Gestion de la fonctionnalité likes et dislikes ---------\\
 //Un clic sur likes ou dislikes = augmentation du compteur et ajout de l'id utilisateur dans la BD
 //Si on re-clic sur likes ou dislikes = reduction du compteur et suppression de l'id user dans la BD
-
-exports.likesDislikes = (req, res, next) => {
-  //si l'utilisateur clic sur likes
-  if (req.body.like === 1){
-    //mise a jour des éléments avec la méthode updateOne pour le userLiked et le compteur de likes
-    Sauce.updateOne({_id: req.params.id}, { $inc: { likes: req.body.like++ }, $push: { usersLiked: req.body.userId }})
-    .then((sauce) => {res.status(200).json({ message: 'Ajout du likes !'});})
-    .catch((error) => {res.status(400).json({ error });});
-    //si l'utilisateur clic sur dislikes
-  } else if (req.body.like === -1){
-    //mise a jour des éléments avec la méthode updateOne pour le userDisliked et le compteur de dislikes
-      Sauce.updateOne({_id: req.params.id}, { $inc: { dislikes: (req.body.like++) * -1 }, $push: { usersDisliked: req.body.userId }})
-      .then((sauce) => {res.status(200).json({ message: 'Ajout du dislikes !'});})
-      .catch((error) => {res.status(400).json({ error });});
-  } else { 
-    //Methodes pour récupérer l'id
-    Sauce.findOne({_id: req.params.id})
-    .then(sauce => {
-      //Si l'utilisateur re-clic sur likes pour annuler son vote
-      if (sauce.usersLiked.includes(req.body.userId)) {
-        //suppression de l'id du user dans la BD usersLiked et on enlève 1 au compteur des likes
-        Sauce.updateOne({_id: req.params.id}, { $inc: { likes: -1 }, $pull: { usersLiked: req.body.userId }})
-        .then((sauce) => {res.status(200).json({ message: 'Suppression du likes !'});})
-        .catch((error) => {res.status(400).json({ error });});
-        //Sinon si l'utilisateur re-clic sur dislikes pour annuler son vote
-      } else if (sauce.usersDisliked.includes(req.body.userId)) {
+exports.likesDislikes = (req, res) => {
+  Sauce.findOne({_id: req.params.id})
+    .then((sauce) => {
+      //modification suite soutenance pour mettre un controle sur le user ID lors du likes ou dislikes
+      //si l'utilisateur clic sur likes et qu'il est pas connu dans likes
+      if (req.body.like === 1 && !sauce.usersLiked.includes(req.body.userId)) {    
+        Sauce.updateOne({_id: req.params.id}, { $inc: {likes: 1}, $push: {usersLiked: req.body.userId}}, {_id: req.params.id})
+          .then(() => res.status(200).json({message: 'Ajout du likes !'}))
+          .catch(error => res.status(400).json({error}));
+      //sinon si l'utilisateur clic sur dislikes et qu'il est pas connu dans dislikes
+      } else if (req.body.like === -1 && !sauce.usersDisliked.includes(req.body.userId)) {  
+          Sauce.updateOne({_id: req.params.id}, { $inc: {dislikes: 1}, $push: {usersDisliked: req.body.userId}}, {_id: req.params.id})
+            .then(() => res.status(200).json({message: 'Ajout du dislikes !'}))
+            .catch(error => res.status(400).json({error}));
+      //Si l'utilisateur re-clic sur likes ou dislikes pour annuler son vote
+      } else if (req.body.like === 0) {  
+          //suppression de l'id du user dans la BD usersLiked et on enlève 1 au compteur des likes     
+          if (sauce.usersLiked.includes(req.body.userId)) {
+            Sauce.updateOne({_id: req.params.id}, {$inc: {likes: -1}, $pull: {usersLiked: req.body.userId}}, {_id: req.params.id})
+              .then(() => res.status(200).json({message: 'Suppression du likes !'}))
+              .catch(error => res.status(400).json({error}));
           //suppression de l'id du user dans la BD usersDisliked et on enlève 1 au compteur des dislikes
-          Sauce.updateOne({_id: req.params.id}, { $inc: { dislikes: -1 }, $pull: { usersDisliked: req.body.userId }})
-          .then((sauce) => {res.status(200).json({ message: 'Suppression du dislikes !'});})
-          .catch((error) => {res.status(400).json({ error });});
+          } else if (sauce.usersDisliked.includes(req.body.userId)) {
+              Sauce.updateOne({_id: req.params.id}, { $inc: {dislikes: -1}, $pull: {usersDisliked: req.body.userId}}, {_id: req.params.id})
+                .then(() => res.status(200).json({message: 'Suppression du dislikes !'}))
+                .catch(error => res.status(400).json({error}));
+          }
       }
     })
-    .catch(error => res.status(500).json({ error }));
-  }
+    .catch(error => {res.status(400).json({error});});
 };
